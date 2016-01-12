66d6b4de45504323120f9df43fbc31f47c5c9d19
App Academy Student <student@appacademy.io>
2016-01-12 12:11:58 -0800

    fix merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
   resources :quizzes,only: [:new]
   resources :quizzes,except: [:new], defaults: {format: :json} do
 
-  collection do
-    get :admin_index, defaults: {format: :html}
-    get ':admin_show/:id', :to => 'quizzes#admin_show', :as => :'admin_show', defaults: {format: :html}
-  end
+    collection do
+      get :admin_index, defaults: {format: :html}
+      get ':admin_show/:id', :to => 'quizzes#admin_show', :as => :'admin_show', defaults: {format: :html}
+    end
 
-  resources :questions, defaults: {format: :html} do
-  # get :admin_show, defaults: {format: :html}
-    resources :answers, defaults: {format: :html}
+    resources :questions, defaults: {format: :html} do
+    # get :admin_show, defaults: {format: :html}
+      resources :answers, defaults: {format: :html}
+    end
+
   end
 
   resources :answer_choices, only: [:create, :index]
