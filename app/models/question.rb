429ada96e18e07d865c9390ef7ca783d7e2ee602
billCaplan <bill.j.caplan@gmail.com>
2016-01-13 23:12:04 -0800

    Charts now render the correct breakdown of question answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,8 @@ class Question < ActiveRecord::Base
   validates :question_type, :quiz_id, :text, :explanation, presence: true
 
   has_many :answers
-  has_many :answer_choices
+  has_many :answer_choices, through: :answers, source: :answer_choices
+  # has_many :given_answers, through: :questions, source: :answers
   belongs_to :quiz
   belongs_to :correct_answer,
     class_name: "Answer",
